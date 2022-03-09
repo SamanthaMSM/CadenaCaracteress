95c3d7f5375ecca60f5de8f15578ee7c20f9d07f
Samantha M. Mendoza <michelle2199_@outlook.com>
2022-03-09 11:35:10 -0800

    Agregar archivos de proyecto.

---
+++ ReadLine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+
+            Console.WriteLine("¿Cuál es su nombre?");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("\nBienvenido " + Nombre + " ¿Desea ver el menú?");
+            Console.ReadKey();
+        }
+    }
+}
